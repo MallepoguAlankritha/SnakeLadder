c3b7e7fe812b62eef64db3b91e5a4493aea94df9
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-09-30 15:53:34 +0530

    position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,32 +6,35 @@ package com.Alankritha;
       
                		int Player_Position=0,dicenum;
 
-        			System.out.println("\nThe Position of the player at starting is " + Player_Position);
-        			dicenum = (int) (Math.floor(Math.random() * 6 % 7 + 1));
+
+        	        for(int i=1; Player_Position <=99;i++)
+        	        {
+        	            System.out.println("\nThe Position of the player at starting is " + Player_Position);
+        	            dicenum = (int) (Math.floor(Math.random() * 6 % 7 + 1));
 
 
-        			int play = (int) (Math.random() * 3);
-        			if (play == 1 || play == 2)
-        				System.out.println("\nThe Number got while rolling dice " + dicenum);
+        	            int play = (int) (Math.random() * 3);
+        	            if (play == 1 || play == 2)
+        	                System.out.println("\nThe Number got while rolling dice " + dicenum);
 
 
-        			switch (play) {
-        			case 0:
-        				System.out.println("\nThere is no play for player, so the positiion is "+Player_Position);
-        				break;
-        			case 1:
-        				Player_Position=Player_Position+dicenum;
-        				System.out.println("\nThe player position get changed to :"+Player_Position);
-        				break;
-        			case 2:
-        				if(Player_Position>dicenum)
-        					Player_Position=Player_Position-dicenum;
-        				else
-        					Player_Position=0;
-        				System.out.println("\nThe player position get changed to :"+Player_Position);
-        				break;
+        	            switch (play) {
+        	                case 0:
+        	                    System.out.println("\nThere is no play for player, so the positiion is " + Player_Position);
+        	                    break;
+        	                case 1:
+        	                    Player_Position = Player_Position + dicenum;
+        	                    System.out.println("\nThe player position get changed to " + Player_Position);
+        	                    break;
+        	                case 2:
+        	                    if (Player_Position > dicenum)
+        	                        Player_Position = Player_Position - dicenum;
+        	                    else
+        	                        Player_Position = 0;
+        	                    System.out.println("\nThe player position get changed to " + Player_Position);
+        	                    break;
+        	            }
 
-        			}
-        		}
-        	}
-
+        	            }
+        	        }
+        	    }
