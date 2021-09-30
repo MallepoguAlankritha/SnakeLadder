79f043b4bca60a6654a3b6c8296db35941d659a7
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-09-30 15:45:58 +0530

    rolling of dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,8 +3,12 @@ package com.Alankritha;
 			public static void main(String[] args) {
 				System.out.println("Welcome to the Snake & Ladder Game");
                        int Player_Position=0;
-				System.out.println("\nThe Position of the player at starting is "+Player_Position);
-				         }
-                    }
+      
+           			System.out.println("\nThe Position of the player at starting is "+Player_Position);
+           			int dicenum=(int)(Math.floor(Math.random() * 6 % 7 +1));
+           			System.out.println("\nThe Number got while rolling dice "+dicenum);
+           		}
+           	}
+
 
 
