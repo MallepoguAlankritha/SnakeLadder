2f20b24aeb9dd397f255d43dae67f9d325244395
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-09-30 15:44:22 +0530

    snakeandladder game

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.Alankritha;
+ public class SnakeLadder {
+			public static void main(String[] args) {
+				System.out.println("Welcome to the Snake & Ladder Game");
+                       int Player_Position=0;
+				System.out.println("\nThe Position of the player at starting is "+Player_Position);
+				         }
+                    }
+
+
